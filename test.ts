1fdbea959d4bb90fad93867a6203509ab7a3823e
Nick Chen <i@nickchen.top>
2026-02-14 16:03:17 +0800

    :wastebasket: remove temp file

--- test.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-
-const foo = z.object({
-  foo: z.string().meta({ description: "bar" }),
-  foobaz: z.string().meta({ description: "barbaz" })
-})
-
-console.log(foo.toJSONSchema())
